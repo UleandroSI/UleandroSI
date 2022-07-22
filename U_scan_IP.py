ef5f4cc9614f689ba71dc4e52d4841070ae7ce07
Leandro Barbosa <uleandrosp7@gmail.com>
2022-07-22 16:43:22 -0300

    Update1

--- U_scan_IP.py
+++ U_scan_IP.py
@@ -12,8 +12,118 @@ import socket
 import os
 import requests
 from tkinter import *
-os.remove("C:/SysTI/IPlivres.txt")
+#os.remove("C:/SysTI/IPlivres.txt")
 IPsLivres = []
+
+
+class Application():
+    def __init__(self, master=None):
+        self.fonte_padrao = ("Arial", "10")
+        
+        self.primeiroConteiner = Frame(master)
+        self.primeiroConteiner["pady"] = 10
+        self.primeiroConteiner.pack()
+
+        self.segundoConteiner = Frame(master)
+        self.segundoConteiner["padx"] = 20
+        self.segundoConteiner.pack()
+
+        self.terceiroConteiner = Frame(master)
+        self.terceiroConteiner["padx"] = 20
+        self.terceiroConteiner.pack()
+
+        self.quartoConteiner = Frame(master)
+        self.quartoConteiner["pady"] = 20
+        self.quartoConteiner.pack()
+
+        master.title("Busca IPs Livres")
+        self.texto_orientacao1 = Label(self.primeiroConteiner, text="Este programa varre o range de IP escolhilho apartir de 10 até 245.")
+        self.texto_orientacao1["font"] = ("Arial", "10", "bold")
+        self.texto_orientacao1.pack()
+
+        self.texto_orientacao2 = Label(self.segundoConteiner, text="Digite o range para o numero de IP. Ex.: 3 = 192.168.3.")
+        self.texto_orientacao2.pack(side= LEFT)
+        self.range = Entry(self.segundoConteiner)
+        self.range["width"] = 30
+        self.range["font"] = self.fonte_padrao
+        self.range.pack(side=LEFT)
+
+        self.botao1 = Button(self.segundoConteiner)
+        self.botao1["text"] = "ENVIAR"
+        self.botao1["font"] = ("Calibri", "8")
+        self.botao1["width"] = "12"
+        self.botao1["command"] = self.verificaEntrada
+        self.botao1.pack()
+
+        self.mensagem = Label(self.terceiroConteiner, text="", font="self.fonte_padrao")
+        self.mensagem.pack()
+
+        self.mensagem2 = Label(self.quartoConteiner, text="", font="self.fonte_padrao")
+        self.mensagem2.pack()
+
+# Funcão de fluxo do programa
+def funcao_principal(self, entrada):
+    self.entrada = entrada
+    retorno_check_nslookup = check_nslookup(entrada)
+    """
+    # Imprimir IPs disponíveis
+    print("")
+    print("IPs disponiveis para uso: ")
+    print("")
+    for ping in IPsLivres:
+        pingstatus = check_ping(ping)
+        if pingstatus == "Network Active":
+            print(pingstatus, ping)
+            IPsLivres.remove(ping)
+        elif pingstatus == "Network Error":
+            print("IP Disponível.",ping)
+        else:
+            print("Erro no ping.")
+
+    salva_arquivo(IPsLivres) """
+
+def verificaEntrada(self):
+    # Recebe o range para scanear
+    self.entrada = self.range.get()
+    if not self.entrada:
+        self.mensagem["text"] = "Digite um número inteiro."
+    else:
+        try:
+            self.entrada = int(self.entrada)
+            # caso usuario digite um range inexistente ele verifica.
+            if self.entrada < 0 or self.entrada > 3:
+                self.mensagem["text"] = "Range deve estar entre 0 e 3."
+            else:
+                self.mensagem["text"] = "Procurando IPs."
+                funcao_principal(self.entrada)
+        except ValueError:
+            self.mensagem["text"] = "É aceito apenas numeros inteiros."
+
+# Função que vefifica se IP está registrado no DNS com nslookup.
+def check_nslookup(self, range):
+    global IPsLivres
+    self.range = range
+    # i é o parametro que conta quantos equipamentos terá por range.
+    i = 210
+    while i < 245:
+        self.comeco = "192.168." # IP ja definido com o começo do endereço.
+        # junta o range + o i do ultimo quadrante.
+        ip = self.comeco + str(self.range) + "." + str(i)
+
+        try:
+            # socket.gethostbyaddr(ip) usa a funcao para encontrar o nome do equipamento registrado no dominio com o IP informado.
+            self.endereco = socket.gethostbyaddr(ip)
+            self.mensagem["text"] = "IP {} - Registrado como {}".format(self.endereco[2], self.endereco[0])
+            #print("IP {} - Registrado como {}".format(self.endereco[2], self.endereco[0]))
+        # caso não encontre o nome do equipamento trata o erro, e imprime qual o IP que falhou.
+        except socket.herror:
+            self.mensagem["text"] = "Endereço: {} não encontrado.".format(ip)
+            #print("Endereço: {} não encontrado.".format(ip))
+            IPsLivres.append(ip)
+            
+        i += 1 # incrementa para avançar o ultimo numero.
+
+#        return self.IPsLivres
 
 # Função para pingar os IPs.
 def check_ping(hostname):
@@ -33,60 +143,12 @@ def salva_arquivo(IPsLivres):
         with open("C:/SysTI/IPlivres.txt", "a") as arquivo:
             arquivo.write("{}\n".format(linhas_do_arquivo))
 
-# Função para receber valor de escaneamento.
-def botao_comecar():
-    while True:
-        # Recebe o range para scanear
-        range = int(input("Digite o range: "))
-        # caso usuario digite um range inexistente ele verifica.
-        if range < 0 or range > 3:
-            print("Range deve estar entre 0 e 3.")
-        else:
-            break
-
-def check_nslookup(IPsLivres):
-    # i é o parametro que conta quantos equipamentos terá por range.
-    i = 210
-    while i < 245:
-        comeco = "192.168." # IP ja definido com o começo do endereço.
-        # junta o range + o i do ultimo quadrante.
-        ip = comeco + str(range) + "." + str(i)
-
-        try:
-            # socket.gethostbyaddr(ip) usa a funcao para encontrar o nome do equipamento registrado no dominio com o IP informado.
-            endereco = socket.gethostbyaddr(ip)
-            print("IP {} - Registrado como {}".format(endereco[2],endereco[0]))
-        # caso não encontre o nome do equipamento trata o erro, e imprime qual o IP que falhou.
-        except socket.herror:
-            print("Endereço: {} não encontrado.".format(ip))
-            IPsLivres.append(ip)
-            
-        i += 1 # incrementa para avançar o ultimo numero.
 
 
-
-def principal():
-    # Imprimir IPs disponíveis
-    print("")
-    print("IPs disponiveis para uso: ")
-    print("")
-    for ping in IPsLivres:
-        pingstatus = check_ping(ping)
-        if pingstatus == "Network Active":
-            print(pingstatus, ping)
-            IPsLivres.remove(ping)
-        elif pingstatus == "Network Error":
-            print("IP Disponível.",ping)
-        else:
-            print("Erro no ping.")
-
-    salva_arquivo(IPsLivres)
-
-janela1 = Tk()
-janela1.title("Busca IPs Livres")
-texto_orientacao1 = Label(janela1, text="Este programa varre o range de IP escolhilho apartir de 10 até 245.").grid(column=1, row=0)
-texto_orientacao2 = Label(janela1, text="Digite o range para o numero de IP. Ex.: 3 = 192.168.3.").grid(column=1, row=1)
-botao1 = Button(janela1, text="ENVIAR").grid(column=2, row=2)
+root = Tk()
+Application(root)
+root.mainloop()
 
 
-janela.mainloop()
+# Melhorias a implementar
+    # Salvar dados no banco de dados.
